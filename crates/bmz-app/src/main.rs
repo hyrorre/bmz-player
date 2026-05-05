3f57a7995baa4ba7cd62b298184107074a6539bb
hyrorre <hiromu.yamashita@hotmail.com>
2026-05-05 23:00:32 +0900

    chore: enable info startup logs

--- crates/bmz-app/src/main.rs
+++ crates/bmz-app/src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 
 fn main() -> Result<()> {
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt().with_max_level(tracing::Level::INFO).init();
     bmz_app::app::run()
 }
